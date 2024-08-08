fea83eed73915cae9a632c0dc9df5d4c5f60ab24
Xavasbekdeveloper <wolfcomp6@gmail.com>
2024-08-08 11:18:13 +0500

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Profile from "./pages/profile/Profile";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useSelector } from "react-redux";
 
 function App() {
+  const token = useSelector((state) => state.auth.token);
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={token ? <Home /> : <Navigate to={"/login"} replace />}
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
